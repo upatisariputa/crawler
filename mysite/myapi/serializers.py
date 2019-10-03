c454ecc803a1387a41e91568c41c4778295eada9
황종옥 <jongock@hwangjong-og-ui-MacBookPro.local>
2019-10-04 00:11:20 +0900

    :art: serialize modified

--- mysite/myapi/serializers.py
+++ mysite/myapi/serializers.py
@@ -15,69 +15,71 @@ from .models import M_video_gap
 class platformSerializer(serializers.ModelSerializer):
     class Meta:
         model = Platform
-        fields = ("id", "name", "type", "url")
+        fields = ("P_key", "P_url", "P_userkey", "P_name")
 
 
 class User_infoSerializer(serializers.ModelSerializer):
     class Meta:
         model = User_info
-        fields = ("id", "U_img", "U_introduce", "U_singup_day", "platform")
+        fields = ("U_key", "U_name", "U_img", "U_info",
+                  "platform", "U_sudate", "P_key")
 
 
 class SubSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subscribe
-        fields = ("id", "update", "count", "platform")
+        fields = ("S_key", "created_at", "S_count",
+                  "year", "month", "week", "day", "P_key")
 
 
 class VideoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Video
-        fields = ("id", "like_count", "unlike_count",
-                  "view_count", "comment_count", "platform")
+        fields = ("V_key", "V_name", "V_upload",
+                  "like_A_Y", "dislike_Y", "view_A_Y_T", "comment_A_Y", "year", "month", "week", "day", "P_key")
 
 
 class TotalSerializer(serializers.ModelSerializer):
     class Meta:
         model = Total
-        fields = ("id", "t_like_count", "t_unlike_count",
-                  "t_view_count", "platform")
+        fields = ("T_key", "T_like_count", "T_unlike_count",
+                  "T_view_count", "T_update", "P_key")
 
 
 class D_sub_gapSerializer(serializers.ModelSerializer):
     class Meta:
         model = D_sub_gap
-        fields = ("id", "update", "count")
+        fields = ("SD_key", "sub_count", "P_key")
 
 
 class W_sub_gapSerializer(serializers.ModelSerializer):
     class Meta:
         model = W_sub_gap
-        fields = ("id", "update", "count")
+        fields = ("SW_key", "sub_count", "P_key")
 
 
 class M_sub_gapSerializer(serializers.ModelSerializer):
     class Meta:
         model = M_sub_gap
-        fields = ("id", "update", "count")
+        fields = ("SM_key", "sub_count", "P_key")
 
 
 class D_video_gapSerializer(serializers.ModelSerializer):
     class Meta:
         model = D_video_gap
-        fields = ("id", "update", "like_count", "unlike_count",
+        fields = ("VD_key", "update", "like_count", "unlike_count",
                   "view_count", "comment_count")
 
 
 class W_video_gapSerializer(serializers.ModelSerializer):
     class Meta:
         model = W_video_gap
-        fields = ("id", "update", "like_count", "unlike_count",
-                  "view_count", "comment_count")
+        fields = ("VW_key", "like_A_Y", "dislike_Y", "view_A_Y_T", "comment_A_Y", "P_key"
+                  )
 
 
 class M_video_gapSerializer(serializers.ModelSerializer):
     class Meta:
         model = M_video_gap
-        fields = ("id", "update", "like_count", "unlike_count",
-                  "view_count", "comment_count")
+        fields = ("VM_key", "like_A_Y", "dislike_Y",
+                  "view_A_Y_T", "comment_A_Y", "P_key")
