8bdf1935d5a0b1f298f9aa16ad145e00740d4615
황종옥 <jongock@hwangjong-og-ui-MacBookPro.local>
2019-10-05 14:01:41 +0900

    :construction: API, view 80% complete data manufacture need

--- mysite/myapi/serializers.py
+++ mysite/myapi/serializers.py
@@ -12,19 +12,11 @@ from .models import W_video_gap
 from .models import M_video_gap
 
 
-class platformSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Platform
-        fields = ["P_key", "P_url", "P_userkey", "P_name"]
-
-
 class User_infoSerializer(serializers.ModelSerializer):
-    Platform = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-
     class Meta:
         model = User_info
         fields = ["U_key", "U_name", "U_img",
-                  "U_info", "U_sudate", "P_key", "Platform"]
+                  "U_info", "U_sudate", "P_key"]
 
 
 class SubSerializer(serializers.ModelSerializer):
@@ -85,3 +77,58 @@ class M_video_gapSerializer(serializers.ModelSerializer):
         model = M_video_gap
         fields = ["VM_key", "like_A_Y", "dislike_Y",
                   "view_A_Y_T", "comment_A_Y", "P_key"]
+
+##################################### realted serializer####################
+
+
+class bjSerializer(serializers.ModelSerializer):
+    Total = TotalSerializer(many=True, read_only=True)
+    User = User_infoSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Platform
+        fields = ["P_key", "P_url", "P_userkey", "P_name", "Total", "User"]
+
+
+class mainSerializer(serializers.ModelSerializer):
+    Sub = SubSerializer(many=True, read_only=True)
+    User = User_infoSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Platform
+        fields = ["P_key", "P_url", "P_userkey", "P_name", "Sub", "User"]
+
+
+class videolistSerializer(serializers.ModelSerializer):
+    Video = VideoSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Platform
+        fields = ["P_key", "P_url", "P_userkey", "P_name", "Video"]
+
+
+class daySerializer(serializers.ModelSerializer):
+    Sub = D_sub_gapSerializer(many=True, read_only=True)
+    SD_gap = D_video_gapSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Platform
+        fields = ["P_key", "P_url", "P_userkey", "P_name", "Sub", "SD_gap"]
+
+
+class weekSerializer(serializers.ModelSerializer):
+    Sub = W_sub_gapSerializer(many=True, read_only=True)
+    SW_gap = W_video_gapSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Platform
+        fields = ["P_key", "P_url", "P_userkey", "P_name", "Sub", "SW_gap"]
+
+
+class monthSerializer(serializers.ModelSerializer):
+    Sub = M_sub_gapSerializer(many=True, read_only=True)
+    SM_gap = M_video_gapSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Platform
+        fields = ["P_key", "P_url", "P_userkey", "P_name", "Sub", "SM_gap"]
--- mysite/myapi/views.py
+++ mysite/myapi/views.py
@@ -1,13 +1,15 @@
-from .serializers import TotalSerializer
 from .models import Total
-from .serializers import User_infoSerializer
 from .models import User_info
-from .serializers import VideoSerializer
 from .models import Video
-from .serializers import SubSerializer
 from .models import Subscribe
-from .serializers import platformSerializer
 from .models import Platform
+from .serializers import mainSerializer
+from .serializers import bjSerializer
+from .serializers import daySerializer
+from .serializers import videolistSerializer
+from .serializers import weekSerializer
+from .serializers import monthSerializer
+
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework import viewsets
@@ -16,92 +18,32 @@ import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "illio.settings")
 
-# Filtered_data.objects.filter(category=category_id["id"], brand=brand)
-# .values("model")
-# .order_by("model")
-# .distinct()
-
 
 class MainViewSet(viewsets.ModelViewSet):
-    # queryset = Platform.objects.filter(P_key=1, P_name="afreeca").values()
-    # queryset = Platform.objects.select_related("User_info")
-
-    # def platform(self, request):
-    #     queryset = Platform.objects.select_related("P_key")
-    #     serializer_class = platformSerializer
-
-    # queryset = Platform.objects.filter(P_name="afreeca")
-    queryset = User_info.objects.all()
-    serializer_class = User_infoSerializer
+    queryset = Platform.objects.all()
+    serializer_class = mainSerializer
 
 
 class BjViewSet(viewsets.ModelViewSet):
-    queryset = User_info.objects.prefetch_related('P_key')
-    serializer_class = User_infoSerializer
-
-    def Bj_info(self, request):
-        # if request.method == "POST":
-        queryset = User_info.objects.filter(P_key=request)
-        serializer_class = User_infoSerializer
-        print(request)
-        # sub = Subscribe.objects.filter(P_key=request)
-        # youtube = Platform.objects.prefetch_related(
-        #     "total").filter(P_key=request).get(P_name=youtube)
-        # twitch = Platform.objects.prefetch_related(
-        #     "total").filter(P_key=request).get(P_name=youtube)
-        # afreeca = Platform.objects.prefetch_related(
-        #     "total").filter(P_key=request).get(P_name=youtube)
-        # result = {"name": user.U_nmae,
-        #           "image": user.U_img,
-        #           "signup-date": user.U_suday,
-        #           "introduce": user.U_info,
-        #           "currnet_sub": {
-        #               "youtube": sub,
-        #               "twitch": sub,
-        #               "afreeca": sub
-        #           },
-        #           "youtube": {
-        #               "total_like": youtube.T_like,
-        #               "total_dislike": youtube.T_dislike,
-        #               "total_view": youtube.T_view
-        #           },
-        #           "twitch": {
-        #               "total_view": twitch.T_view
-        #           },
-        #           "afreeca": {
-        #               "total_like": afreeca.T_like,
-        #               "total_view": afreeca.T_view
-        #           }
-        #           }
+    queryset = Platform.objects.all()
+    serializer_class = bjSerializer
 
 
-class SubListViewSet(viewsets.ModelViewSet):
-    queryset = Subscribe.objects.all().select_related("platform")
-    serializer_class = SubSerializer
+class VideolistViewSet(viewsets.ModelViewSet):
+    queryset = Platform.objects.all()
+    serializer_class = videolistSerializer
 
 
-class BjListViewSet(viewsets.ModelViewSet):
-    queryset = Video.objects.all()
-    serializer_class = VideoSerializer
+class DayViewSet(viewsets.ModelViewSet):
+    queryset = Platform.objects.all()
+    serializer_class = daySerializer
 
 
-class MaxLikeVideo(viewsets.ModelViewSet):
-    queryset = Video.objects.all().order_by("like_count")
-    serializer_class = VideoSerializer
+class WeekViewSet(viewsets.ModelViewSet):
+    queryset = Platform.objects.all()
+    serializer_class = weekSerializer
 
 
-class MinLikeVideo(viewsets.ModelViewSet):
-    queryset = Video.objects.all().order_by("-like_count")
-    serializer_class = VideoSerializer
-
-
-class MaxViewCount(viewsets.ModelViewSet):
-    queryset = Video.objects.all().order_by("view_count")
-    serializer_class = VideoSerializer
-
-
-class MinViewCount(viewsets.ModelViewSet):
-    queryset = Video.objects.all().order_by("view_count")
-    serializer_class = VideoSerializer
-
-# def MaxGapYotubeViewSet(request,)
+class MonthViewset(viewsets.ModelViewSet):
+    queryset = Platform.objects.all()
+    serializer_class = monthSerializer
