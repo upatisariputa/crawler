6a88468be6e174788d33454f7932752c174c0279
onedanbee <onedanbee@gmail.com>
2019-10-06 14:36:39 +0900

    :sparkles: Add function auth

--- mysite/myapi/views.py
+++ mysite/myapi/views.py
@@ -14,6 +14,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework import viewsets
 from rest_framework.decorators import api_view
+from rest_framework import permissions
 import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "illio.settings")
@@ -22,28 +23,34 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "illio.settings")
 class MainViewSet(viewsets.ModelViewSet):
     queryset = Platform.objects.all()
     serializer_class = mainSerializer
+    permission_classes = [permissions.IsAdminUser]
 
 
 class BjViewSet(viewsets.ModelViewSet):
     queryset = Platform.objects.all()
     serializer_class = bjSerializer
+    permission_classes = [permissions.IsAdminUser]
 
 
 class VideolistViewSet(viewsets.ModelViewSet):
     queryset = Platform.objects.all()
     serializer_class = videolistSerializer
+    permission_classes = [permissions.IsAdminUser]
 
 
 class DayViewSet(viewsets.ModelViewSet):
     queryset = Platform.objects.all()
     serializer_class = daySerializer
+    permission_classes = [permissions.IsAdminUser]
 
 
 class WeekViewSet(viewsets.ModelViewSet):
     queryset = Platform.objects.all()
     serializer_class = weekSerializer
+    permission_classes = [permissions.IsAdminUser]
 
 
 class MonthViewset(viewsets.ModelViewSet):
     queryset = Platform.objects.all()
     serializer_class = monthSerializer
+    permission_classes = [permissions.IsAdminUser]
